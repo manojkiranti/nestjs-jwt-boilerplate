710a79853f5e2468d2cbaa2623f2f3e631301bdb
manojkiranti <manoj.kiranti@gmail.com>
2023-08-10 16:27:47 +0545

    refactor code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { QuizModule } from './modules/quiz/quiz.module';
-import { BranchModule } from './branch/branch.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfigAsync } from './config/typeorm.config';
 import { ConfigModule } from '@nestjs/config/dist';
@@ -12,7 +11,6 @@ import { AuthModule } from './modules/auth/auth.module';
     ConfigModule.forRoot({ isGlobal: true, envFilePath: `.env` }),
     TypeOrmModule.forRootAsync(typeOrmConfigAsync),
     QuizModule,
-    BranchModule,
     UsersModule,
     AuthModule,
   ],
